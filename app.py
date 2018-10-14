83a0bd292983e615ec9e1ff5b78c61b8a348308d
Aditya Bhardwaj <aditya.1998bhardwaj@gmail.com>
2018-10-14 18:55:43 +0000

    Final Commit

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def symptom():
             'ID':i['ID']
         }
         A.append(res)
-    return json_response(ID)
+    return json_response(A)
     
 @app.route('/diagnose')
 def diagnosepage():
