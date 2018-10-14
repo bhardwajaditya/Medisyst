1bd37367fa67adda3ca255dca541cdb182e6b52a
Aditya Bhardwaj <aditya.1998bhardwaj@gmail.com>
2018-10-14 18:51:25 +0000

    doctor profile added

--- app.py
+++ app.py
@@ -33,9 +33,7 @@ addrequests=db.addrequests
 # doctors.delete_many({})
 # addrequests.delete_many({})
 
-x=doctors.find()
-for i in x:
-    print(i)
+
 
 username = "aditya.1998bhardwaj@gmail.com"
 password = "Wd85RaFy76Xrw3QBx"
@@ -62,7 +60,7 @@ def symptom():
             'ID':i['ID']
         }
         A.append(res)
-    return json_response(A)
+    return json_response(ID)
     
 @app.route('/diagnose')
 def diagnosepage():
